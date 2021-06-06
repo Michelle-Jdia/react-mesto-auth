f6cae1d283aaa996d0a547af51211b14610ab366
Michelle Jdia <mj.sy.ru@gmail.com>
2021-06-06 20:13:30 +0300

    Updates

--- src/components/Footer.js
+++
@@ -1,9 +0,0 @@
-import React from "react";
-export default Footer;
-function Footer() {
-  return (
-    <footer className="footer page__footer">
-      <p className="footer__copyright">{`© 2020 - ${new Date().getFullYear()} Mesto Russia`}</p>
-    </footer>
-  );
-}
